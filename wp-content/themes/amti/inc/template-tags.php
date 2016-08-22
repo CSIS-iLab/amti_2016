cea6c90905f3638e5680a67ab2930bb6afc60dc7
Paul Franz <paulkfranz@gmail.com>
2016-08-22 17:24:19 -0400

    coauthors feature branch init commit

--- wp-content/themes/amti/inc/template-tags.php
+++ wp-content/themes/amti/inc/template-tags.php
@@ -11,32 +11,16 @@ if ( ! function_exists( 'transparency_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
  */
-function transparency_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
 
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
+/* Rewrote/simplified get_the_coauthor function -PF */
 
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'transparency' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
-
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'transparency' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
-
-}
+ function transparency_posted_on() {
+ echo 'Posted on <a href="' . get_permalink() . '">' . get_the_date() . '</a> by&nbsp;';
+ if ( function_exists( 'coauthors_posts_links' ) )
+ coauthors_posts_links();
+ else
+ the_author();
+ }
 endif;
 
 if ( ! function_exists( 'transparency_entry_footer' ) ) :
