83e17132bc68b14ede3f60c9fec5d4e33d0d3309
Jacque Schrag <jschrag@Jacquelines-iMac.local>
2016-08-24 15:21:54 -0400

    Update footer & install font awesome

--- wp-content/themes/amti/functions.php
+++ wp-content/themes/amti/functions.php
@@ -108,6 +108,9 @@ function transparency_scripts() {
 
 	wp_enqueue_script( 'transparency-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	// Font Awesome
+	wp_enqueue_script('transparency-font-awesome', 'https://use.fontawesome.com/08b1a76eab.js');
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
