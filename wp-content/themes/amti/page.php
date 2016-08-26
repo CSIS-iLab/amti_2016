33addd15c414f66c3d47f926a095b60a45bb0ddd
Jacque Schrag <jschrag@Jacquelines-iMac.local>
2016-08-26 10:59:04 -0400

    Updated page.php structure

--- wp-content/themes/amti/page.php
+++ wp-content/themes/amti/page.php
@@ -1,38 +1,29 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying all single posts.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package Transparency
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="container">
+		<div class="row">
+			<main id="main" class="col-xs-12" role="main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+				<?php
+				while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+					get_template_part( 'template-parts/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+				endwhile; // End of the loop.
+				?>
 
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
+			</main><!-- #main -->
+		</div><!-- .row -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
