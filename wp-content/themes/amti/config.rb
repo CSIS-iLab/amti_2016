580f8697a7a83356deb8d7a1268fe7eb716e320a
Jacque Schrag <jschrag@Jacquelines-iMac.local>
2016-09-13 17:18:25 -0400

    Updated compass config to hide comments

--- wp-content/themes/amti/config.rb
+++ wp-content/themes/amti/config.rb
@@ -6,9 +6,11 @@ images_dir = "images" #targets our pre existing image directory
 javascripts_dir = "js" #targets our JavaScript directory
  
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compact
+output_style = :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
  
 # To enable relative paths to assets via compass helper functions.
 # note: this is important in wordpress themes for sprites
- 
 relative_assets = true
