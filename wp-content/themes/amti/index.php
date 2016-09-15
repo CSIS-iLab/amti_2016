360c5aeb31a1af2ff0cb48afe83d18b2558be31f
Jacque Schrag <jschrag@Jacquelines-iMac.local>
2016-09-15 11:22:04 -0400

    Updated Archive Listing
    - Removed home.php and moved theming to index.php
    - Formatted archive layout

--- wp-content/themes/amti/index.php
+++ wp-content/themes/amti/index.php
@@ -20,44 +20,47 @@ get_header(); ?>
 	</div><!-- #homepage-slider-container -->
 <?php endif; ?>
 
+	<div id="primary" class="container posts-index archives-index">
+		<header class="entry-header">
+			<h1 class="page-title"><?php single_post_title( ); ?></h1>
+			<hr>
+			<p class='title-description'>Browse all of our analysis.</p>
+		</header><!-- .entry-header -->
+		<div class="row">
+			<main id="main" class="col-xs-12 col-md-9" role="main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+				<?php
+				if ( have_posts() ) :
+				?>
 
-		<?php
-		if ( have_posts() ) :
+					<?php
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
 
-			<?php
-			endif;
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+					endwhile;
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+					the_posts_navigation(array('prev_text' => '<i class="fa fa-angle-left" aria-hidden="true"></i> Older Posts', 'next_text' => 'Newer Posts <i class="fa fa-angle-right" aria-hidden="true"></i>'));
 
-			endwhile;
+				else :
 
-			the_posts_navigation();
+					get_template_part( 'template-parts/content', 'none' );
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+				endif; ?>
 
 		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php
+			get_sidebar();
+		?>
+	</div><!-- .row -->
+</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
