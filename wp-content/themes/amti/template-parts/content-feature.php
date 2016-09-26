576874a68e8b9c69a665108b3d82adf913935a6b
Paul Franz <paulkfranz@gmail.com>
2016-09-26 17:16:55 -0400

    features index listing updated

--- wp-content/themes/amti/template-parts/content-feature.php
+++ wp-content/themes/amti/template-parts/content-feature.php
@@ -10,32 +10,59 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<div class="row">
+		<div class="col-xs-2">
+			<?php echo get_the_post_thumbnail( $_post->ID, 'thumbnail' ); ?>
+		</div>
+		<div class="col-xs-10">
+	<header>
 		<?php
-
-		// Get post meta
-		$the_excerpt = get_the_excerpt();
-		$terms = get_the_terms( $post->ID , 'categories' );
 
 		// Echo title, date, excerpt, and featured image
 		if ( is_single() ) :
-		  the_title( '<h1 class="entry-title">', '</h1>' );
-		  else :
-		    the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</h2>' );
-		    echo '<div style="float: right;">' . get_the_post_thumbnail( $_post->ID, 'thumbnail' ) . '</a></div><br/><b>' . get_the_date() . ' | </b>' .$the_excerpt;
-		  endif;
+			the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</h2></a>' );
+		endif;
 
-		  // Echo the feature categories
-		    echo '<br />Posted in: ';
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+
+			<?php
+
+				$terms = get_the_terms( $post->ID , 'categories' );
+				echo '<br />Posted in: ';
 		    foreach ( $terms as $term ) {
 		                    $term_link = get_term_link( $term, 'features' );
 		                    if( is_wp_error( $term_link ) )
 		                    continue;
 		                echo '<a href="' . $term_link . '">' . $term->name . '&nbsp;</a>';
-		  } ?>
+		  }
+				?>
+
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
 	</header><!-- .entry-header -->
 
-	<footer class="entry-footer">
-		<?php transparency_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+		<div class="entry-meta">
+		<?php
+		$terms = get_the_terms( $post->ID , 'categories' );
+		echo get_the_date();
+		echo ' | ';
+		echo 'Category: ';
+		foreach ( $terms as $term ) {
+										$term_link = get_term_link( $term, 'features' );
+										if( is_wp_error( $term_link ) )
+										continue;
+								echo '<a href="' . $term_link . '">' . $term->name . '&nbsp;</a>';
+	}
+			echo '<div class="entry-content">';
+			echo get_the_excerpt();
+			echo '</div>';
+		?>
+		</div>
+	</div><!-- .entry-content -->
+</div>
+</div>
 </article><!-- #post-## -->
