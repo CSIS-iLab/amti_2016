a9c4537b7b848240fccd9405612a39b03b11662b
Jacque Schrag <jschrag@Jacquelines-iMac.local>
2016-09-30 15:14:50 -0400

    Remove explore, update features pagination

--- wp-content/themes/amti/template-parts/content-island-tracker-single.php
+++ wp-content/themes/amti/template-parts/content-island-tracker-single.php
@@ -14,7 +14,7 @@
 		<?php
 		// Echo title
 		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">Explore ', '</h1>' );
+			the_title( '<h1 class="entry-title">', '</h1>' );
 			echo "<hr>";
 			else :
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</h2>' );
