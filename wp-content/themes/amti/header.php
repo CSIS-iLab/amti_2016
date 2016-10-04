5a5d8f9dd638c626d6901b85d80e2c107adda846
Jacque Schrag <jnschrag@gmail.com>
2016-10-04 10:43:08 -0400

    Move google analytics to plugin file

--- wp-content/themes/amti/header.php
+++ wp-content/themes/amti/header.php
@@ -17,17 +17,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-55717458-2', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
 <?php wp_head(); ?>
 </head>
 
