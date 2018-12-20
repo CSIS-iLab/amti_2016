50ec0e03433760011d8947ec7e6146046b365b81
leezaweb <leeza.web@gmail.com>
2018-12-20 18:47:19 -0500

    remove another spread operator

--- wp-content/themes/amti/settings-page/settings-page.js
+++ wp-content/themes/amti/settings-page/settings-page.js
@@ -2,9 +2,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var select = document.getElementById(
     "transparency_homepage_featured_satellites"
   );
+
+  if (!select) {
+    return;
+  }
+
   select.setAttribute(
     "data-originalValue",
-    [...select.selectedOptions].map(function(option) {
+    Array.from(select.selectedOptions).map(function(option) {
       return option.value;
     })
   );
@@ -12,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (this.selectedOptions.length > 5) {
       alert("Choose 5 or Fewer Maps");
       let selectedIds = select.getAttribute("data-originalValue").split(",");
-      [...select.options]
+      Array.from(select.options)
         .filter(function(option) {
           option.selected = false;
           return selectedIds.includes(option.value);
