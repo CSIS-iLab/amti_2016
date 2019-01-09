007aeaeb54168aec215394eca500109f99293ae3
leezaweb <leeza.web@gmail.com>
2019-01-09 09:40:27 -0500

    'Photo' language

--- wp-content/themes/amti/template-parts/homepage_slider.php
+++ wp-content/themes/amti/template-parts/homepage_slider.php
@@ -38,7 +38,13 @@ echo '</div></div></div>';
 
 
    echo '<div class="island-slideshow-caption">
-       <span class="feature-subtitle">'.__('Photo', 'transparency').':</span>';
+       <span class="feature-subtitle">';
+ 		?>
+
+    <?php echo __('Photo:', 'transparency')?>
+
+
+    <?php echo '</span>';
 
     foreach ($satellites as $key=>$post) : setup_postdata($post);
        if ($key == 0) {
