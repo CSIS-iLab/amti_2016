a4c3fed9f151a10083e584299ac4466722f5a3cc
leezaweb <leeza.web@gmail.com>
2019-01-10 11:57:29 -0500

    create maps page template

--- wp-content/themes/amti/template-parts/content-page.php
+++ wp-content/themes/amti/template-parts/content-page.php
@@ -19,5 +19,28 @@
 		<?php
 			the_content();
 		?>
-	</div><!-- .entry-content -->
+		<?php
+		    global $related;
+		    $rel = $related->show( get_the_ID(), true );
+
+		    // Display the title of each related post
+		    if( is_array( $rel ) && count( $rel ) > 0 ) {
+					echo '<div id="mapContainer">';
+		        foreach ( $rel as $r ) {
+		            if ( is_object( $r ) ) {
+		                if ($r->post_status != 'trash') {
+											$image = wp_get_attachment_image_src(get_post_thumbnail_id($r->ID), $size = 'large')[0];
+
+		                    echo '<a href="' . esc_url(get_permalink($r->ID)) . '" class="map">
+												<span class="background" style="background-image:url('.$image .'");>
+												</span>
+												<span class="title">' . get_the_title($r->ID) . '</span>
+												</a>';
+
+		                }
+		            }
+		        }
+						echo '</div>';
+		    }
+		?>	</div><!-- .entry-content -->
 </article><!-- #post-## -->
