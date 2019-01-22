086fe3fb5978b5cad82af62d3fc985e60d30feab
leezaweb <leeza.web@gmail.com>
2019-01-22 11:25:59 -0500

    adjust island tracker

--- wp-content/themes/amti/archive-island-tracker.php
+++ wp-content/themes/amti/archive-island-tracker.php
@@ -14,46 +14,43 @@ get_header(); ?>
 			<main id="main" class="col-xs-12" role="main">
 				<header class="page-header">
 					<?php
-					echo "<h1 class='page-title'>".esc_html__('Occupation and Island Building', 'transparency')."</h1>";
-					echo "<hr>";
-					echo "<p>".esc_html__("Five claimants occupy nearly 70 disputed reefs and islets spread across the South China Sea. They have built more than 90 outposts on these contested features, many of which have seen expansion in recent years. AMTI has gathered satellite imagery of each outpost, along with other relevant information, to document their current status and any changes they have undergone in recent years. Explore the database below.", 'transparency')."</p>";
-					?>
+                    echo "<h1 class='page-title'>".esc_html__('Occupation and Island Building', 'transparency')."</h1>";
+                    echo "<hr>";
+                    echo "<p>".esc_html__("Five claimants occupy nearly 70 disputed reefs and islets spread across the South China Sea. They have built more than 90 outposts on these contested features, many of which have seen expansion in recent years. AMTI has gathered satellite imagery of each outpost, along with other relevant information, to document their current status and any changes they have undergone in recent years. Explore the database below.", 'transparency')."</p>";
+                    ?>
 				</header><!-- .page-header -->
 				<div class="row">
-					<?php 
-						$args=array(
-						  'name' => 'countries'
-						);
-						$output = 'objects'; // or names
-						$taxonomies=get_taxonomies($args,$output);
+					<?php
+                        $args=array(
+                          'name' => 'countries'
+                        );
+                        $output = 'objects'; // or names
+                        $taxonomies=get_taxonomies($args, $output);
 
-						if ($taxonomies) {
-						  foreach ($taxonomies  as $taxonomy ) {
-						  	$terms = get_terms($taxonomy->name, array('parent' => 0));
-		        			foreach ( $terms as $term) {
+                        if ($taxonomies) {
+                            echo '<div id="islandTrackerContainer">';
 
+                            foreach ($taxonomies  as $taxonomy) {
+                                $terms = get_terms($taxonomy->name, array('parent' => 0));
+                                foreach ($terms as $term) {
+                                    $image = get_term_meta($term->term_id, 'countries_feature_image', true);
 
-		        				$image = get_term_meta( $term->term_id, 'countries_feature_image', true );
+                                    if (function_exists('icl_object_id') && ICL_LANGUAGE_CODE != "en") {
+                                        $langQuery = "/?lang=".ICL_LANGUAGE_CODE;
+                                    } ?>
 
-		        				if(function_exists('icl_object_id') && ICL_LANGUAGE_CODE != "en") {
-		        					$langQuery = "/?lang=".ICL_LANGUAGE_CODE;
-		        				}
+									<?php	echo '<a href="' . $term->slug.$langQuery . '" class="country-listing">
+					 			 <span class="background" style="background-image:url('.$image .'");>
+					 			 </span>
+					 			 <span class="title">' . $term->name . '</span>
+					 			 </a>';
 
-		        			?>
-
-		        			<div class="col-xs-12 col-sm-6">
-		        				<a href="<?php echo $term->slug.$langQuery; ?>">
-			        				<div class="country-listing" style="background-image:url('<?php echo $image; ?>');">
-			        					<div class="title"><?php echo $term->name; ?></div>
-			        				</div>
-			        			</a>
-		        			</div>
-
-		        			<?
-		        			}
-						  }
-						}  
-					?>
+		        
+                                }
+                            }
+                            echo '</div>';
+                        }
+                    ?>
 				</div>
 			</main><!-- #main -->
 		</div>
